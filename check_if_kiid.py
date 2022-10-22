ac82f39f56fa06a7118b72c2a08469e11ff53674
Damian Skowroński <72418134+skowronskid@users.noreply.github.com>
2022-10-22 12:19:11 +0200

    added check_if_kiid.py

---
+++ check_if_kiid.py
@@ -0,0 +1,16 @@
+import PyPDF2 as pdf
+
+def check_kiid(file_name):
+    # podajesz nazwe pliku i zwraca TRUE gdy tekst zaczyna sie od "kluczowe informacje dla inwestorow"
+
+    file = open(file_name,'rb')
+    reader = pdf.PdfFileReader(file)
+    pageObj = reader.getPage(0)
+    text = pageObj.extract_text()
+    text_ = text.lower()
+    return text_.startswith("kluczowe informacje dla inwestorów")   
+
+if __name__ == "__main__":
+    #test
+    file_name = "KIID_BNP_Paribas_DI_2022-09-21.pdf" 
+    print(check_kiid(file_name))
