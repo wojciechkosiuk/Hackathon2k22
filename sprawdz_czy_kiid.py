28e1eb28286d046354adf1329769d51d3b814670
amajczyk <72541777+amajczyk@users.noreply.github.com>
2022-10-22 20:04:49 +0200

    Add files via upload

---
+++ sprawdz_czy_kiid.py
@@ -0,0 +1,58 @@
+import os
+import pdfplumber
+
+if not os.path.isdir("KIID"):
+    os.mkdir("KIID")
+
+def check_kiid(file_name):
+    # podajesz nazwe pliku i zwraca TRUE gdy tekst zaczyna sie od "kluczowe informacje dla inwestorow"
+
+    pdf = pdfplumber.open(file_name)
+    text = pdf.pages[0].extract_text().lower()[1:200]
+    return "kluczowe informacje dla inwestorów" in text
+
+
+if __name__ == "__main__":
+    listaFolderow = []
+    dir_path= '/Users/adammajczyk/PycharmProjects/websraping/download/'
+    for path in os.listdir(dir_path):
+        # check if current path is a file
+        if not os.path.isfile(os.path.join(dir_path, path)):
+            listaFolderow.append(path)
+
+
+
+    listaPlikow=[]
+    for folder in listaFolderow:
+        for path in os.listdir(dir_path + folder):
+            # check if current path is a file
+            if  os.path.isfile(os.path.join(dir_path + folder, path)):
+                listaPlikow.append(dir_path + folder + "/" + path)
+
+
+    if (len(listaPlikow)>0):
+        kiids = []
+        i = 1
+        for plik in listaPlikow:
+            print (str(i)+"/"+str(len(listaPlikow)),":", plik, end = " ")
+            try:
+                if (check_kiid(plik)):
+
+                    nazwa_pliku=plik.split("/")[len(plik.split("/"))-1]
+                    os.rename(plik, dir_path+"/KIID/" + nazwa_pliku)
+
+                    kiids.append(plik)
+                    print("to KIID", end="")
+                else:
+                    print("to nie KIID - usuwam", end = "")
+                    os.remove(plik)
+            except:
+                os.remove(plik)
+                print("blad - usuwam")
+            i+=1
+            print("")
+
+
+        print(len(kiids))
+    else:
+        print("jest tyle samo plikow")
---
+++ zdobywanie_linkow.py
@@ -0,0 +1,132 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import json
+import time
+
+urls = [
+'www.aliortfi.com',
+'www.amundi.pl',
+'www.baltictfi.pl',
+'www.tfi.bnpparibas.pl',
+'www.bpstfi.pl',
+'www.caspartfi.pl',
+'www.eitfi.pl',
+'www.esaliens.pl',
+'www.generali-investments.pl',
+'www.investors.pl',
+'www.ipopema.pl',
+'www.millenniumtfi.pl',
+'www.metlife.pl',
+'www.nntfi.pl',
+'www.noblefunds.pl',
+'www.opera-tfi.pl',
+'www.pekaotfi.pl',
+'www.pfrtfi.pl',
+'www.pkotfi.pl',
+'www.qtfi.pl',
+'www.rockbridge.pl',
+'www.santandertfi.pl',
+'www.skarbiec.pl',
+'www.superfund.pl',
+'www.franklintempleton.pl',
+'www.tfiagro.pl',
+'www.allianz.pl',
+'www.tfienergia.pl',
+'www.pzu.pl',
+'www.uniqa.pl',
+'www.vigcq-tfi.pl'
+]
+
+url = ""
+dict_href_links = {}
+
+
+def getdata(url):
+    r = requests.get(url)
+    return r.text
+
+
+def get_links(website_link):
+    html_data = getdata(website_link)
+    soup = BeautifulSoup(html_data, "html.parser")
+    list_links = []
+    for link in soup.find_all("a", href=True):
+
+        # Append to list if new link contains original link
+        if str(link["href"]).startswith((str(website_link))):
+            list_links.append(link["href"])
+
+        # Include all href that do not start with website link but with "/"
+        if str(link["href"]).startswith("/"):
+            if link["href"] not in dict_href_links:
+                print(link["href"])
+                dict_href_links[link["href"]] = None
+                link_with_www = website_link + link["href"][1:]
+                print("adjusted link =", link_with_www)
+                list_links.append(link_with_www)
+
+    # Convert list of links to dictionary and define keys as the links and the values as "Not-checked"
+    dict_links = dict.fromkeys(list_links, "Not-checked")
+    return dict_links
+
+
+def get_subpage_links(l):
+    noww = time.perf_counter()
+    for link in tqdm(l):
+        if(time.perf_counter()-noww>45):
+            print("\n\n\n zwracane jest bq\n\n\n")
+            return l
+        print("link: " + link)
+        # If not crawled through this page start crawling and get links
+        if l[link] == "Not-checked":
+            dict_links_subpages = get_links(link)
+            # Change the dictionary value of the link to "Checked"
+            l[link] = "Checked"
+        else:
+            # Create an empty dictionary in case every link is checked
+            dict_links_subpages = {}
+        # Add new dictionary to old dictionary
+        l = {**dict_links_subpages, **l}
+    return l
+
+gigalista = {}
+for url in urls:
+    # If there is no such folder, the script will create one automatically
+    folder_location = r"/Users/adammajczyk/PycharmProjects/websraping/download/" + url[7:17:1]
+    if not os.path.exists(folder_location): os.mkdir(folder_location)
+
+    dict_href_links = get_links(url)
+    # add websuite WITH slash on end
+    website = url
+    # create dictionary of website
+    dict_links = {website: "Not-checked"}
+
+    counter, counter2 = None, 0
+    start = time.perf_counter()
+    while counter != 0:
+        counter2 += 1
+        dict_links2 = get_subpage_links(dict_links)
+        # Count number of non-values and set counter to 0 if there are no values within the dictionary equal to the string "Not-checked"
+        # https://stackoverflow.com/questions/48371856/count-the-number-of-occurrences-of-a-certain-value-in-a-dictionary-in-python
+        counter = sum(value == "Not-checked" for value in dict_links2.values())
+        # Print some statements
+        print("")
+        print("THIS IS LOOP ITERATION NUMBER", counter2)
+        print("LENGTH OF DICTIONARY WITH LINKS =", len(dict_links2))
+        print("NUMBER OF 'Not-checked' LINKS = ", counter)
+        print("")
+        dict_links = dict_links2
+        # Save list in json file
+
+        now = time.perf_counter()
+        if (now - start > 300):
+            print("\n\n\n wywalane jest\n\n\n")
+            break
+
+    gigalista = gigalista | dict_links
+
+a_file = open("data.json", "a")
+json.dump(gigalista, a_file)
+a_file.close()
