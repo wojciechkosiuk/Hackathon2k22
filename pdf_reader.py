e7ed8ee3614509441c961add4e8b997a5cc1b3fe
Damian Skowroński <72418134+skowronskid@users.noreply.github.com>
2022-10-22 23:02:41 +0200

    dodane od kosiuka

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,7 +1,8 @@
 import re
 import pdfplumber
 import pandas as pd
-# from stempel import StempelStemmer
+import numpy as np
+from stempel import StempelStemmer
 
 
 
@@ -20,12 +21,114 @@ def extract(file_name):
     return full_text
 
 
-def get_list_of_currencies():
-    # returns df of abbreviations and iso of currencies
-    currencies = pd.read_csv("List_of_circulating_currencies_1.csv",sep=',')[['Symbol or\nAbbrev.', 'ISO code']]
-    currencies.columns = ['abbr','iso']
-    currencies.drop_duplicates(subset = "abbr", inplace = True)
-    return currencies
+
+    
+
+def _3(full_text):
+    start_vec = full_text.split("inwestycyjną")[1].split("\n")
+    NAZWA_SUBFUNDUSZU = [x for x in start_vec  if len(x)>2][0]
+    return NAZWA_SUBFUNDUSZU
+
+
+def _8(full_text):
+    text_stemmed = full_text.lower().split("cele i polityka inwestycyjna")[0].split()
+    tmp = [stemmer.stem(x)=="zarządzać" for x in full_text.lower().split("cele i polityka inwestycyjna")[0].split()]
+
+    df = pd.DataFrame({'text':text_stemmed, 'boolvec':tmp})
+    word = df[df['boolvec']==True]['text']
+
+    if word.empty:
+        return None
+    else:
+        stemmed = full_text.split(np.array(word)[0])[1].split("\n")[0].split('S.A.')[0] + "S.A."
+        upper_words = [x[0].isupper() for x in stemmed.split()]
+        words = np.array(stemmed.split())[upper_words]
+        return " ".join(words)
+
+
+def _9(full_text):
+    DATA_AKTUALIZACJI_KIID = full_text.split("dzień")[-1].split("\n")[0]
+    return DATA_AKTUALIZACJI_KIID
+
+
+def _26(full_text):
+    OPLATA_ZA_NABYCIE = full_text.lower().split("opłata za nabycie")[1].split()[0]
+    return OPLATA_ZA_NABYCIE
+
+
+def _27(full_text):
+    if len(full_text.lower().split("opłata za odkupienie"))==1:
+        if len(full_text.lower().split("opłata za umorzenie"))==1:
+            return None
+        else:
+            OPLATA = full_text.lower().split("opłata za umorzenie")[1].split()[0]
+    else:
+        OPLATA = full_text.lower().split("opłata za odkupienie")[1].split()[0]
+    return OPLATA
+
+
+
+def _28(full_text):
+    try:
+        OPLATY_BIEZACE = full_text.lower().split("opłaty bieżące")[1].split()[0]
+    except:
+        return None
+    return OPLATY_BIEZACE
+
+
+def _29(full_text):
+    try:
+        OPLATY_ZA_WYNIKI = full_text.lower().split("opłaty za wyniki")[1].split()[0]
+    except:
+        return None
+    return OPLATY_ZA_WYNIKI 
+
+
+def _3150(full_text): 
+    #bo od 31 do 50
+    if len(re.split(r'\n[0-9]{4} ',full_text))<2:
+        return None
+    
+    tmp = re.split(r'\n[0-9]{4} ',full_text)[1].split("\n")[0:6]
+    tmp = [re.sub("[a-z]|[A-Z]", "", x) for x in tmp]
+    tmp = [re.sub("[.?!]", "", x) for x in tmp]
+    years = re.findall(r"[0-9]{4}",tmp[0])
+    values = []
+    for row in tmp[1:]:
+        if len(re.findall('%',row))!=0:
+            values.append(row)
+
+    years.insert(0,int(years[0])-1)
+
+    values = [x.split() for x in values]
+    return pd.DataFrame({'Years':years,'Values':values[0],'Values_benchmark':values[1]})
+
+
+
+def _52(full_text):
+    try:
+        name = full_text.lower().split("depozytariuszem")[1].split(". ")[0].split("jest")[1]
+    except:
+        return None
+    return name
+
+
+
+def _53(full_text):
+    try:
+        krs = re.findall(r'[0-9]{10}',full_text.lower().split("informacje praktyczne")[1])
+    except:
+        return None
+    return krs
+
+
+def _54(full_text):
+    try:
+        nip = re.findall(r'[0-9]{3}-[0-9]{2}-[0-9]{2}-[0-9]{3}',full_text.lower().split("informacje praktyczne")[1])
+    except:
+        return None
+    return nip
+
 
 
 def _55(full_text,stemmer):
@@ -50,10 +153,18 @@ def _56(full_text):
     except:
         return None
 
-def _57(full_text):
+def get_list_of_currencies():
+    # returns df of abbreviations and iso of currencies
+    # used in _57()
+    currencies = pd.read_csv("List_of_circulating_currencies_1.csv",sep=',')[['Symbol or\nAbbrev.', 'ISO code']]
+    currencies.columns = ['abbr','iso']
+    currencies.drop_duplicates(subset = "abbr", inplace = True)
+    return currencies
+
+def _57(full_text,currencies):
     # returnuje walute kapitału (ostatni akapit)
     try:
-        currencies = get_list_of_currencies()
+        # currencies = get_list_of_currencies()
         inf_prak = full_text.lower().split("informacje praktyczne")[1].split()
         indices = [i for i, x in enumerate(inf_prak) if x in currencies.abbr.values]
         for i in indices:
@@ -79,12 +190,8 @@ def _59(full_text):
 
 
 def main():
-    # test
-    # file_name = "be4dbbf2-4e25-4053-87ba-52f13e939a48.pdf"
-    # text = extract(file_name) 
-    # print(text)
-    
-    pass
+    stemmer = StempelStemmer.polimorf()
+    currencies = get_list_of_currencies()
     
 
 if __name__ == "__main__":
