8eb9cfdf7389379c0abf761c40a0249aa5fe9e0b
Damian Skowroński <72418134+skowronskid@users.noreply.github.com>
2022-10-22 17:23:25 +0200

    podmianka

--- check_if_kiid.py
+++ check_if_kiid.py
@@ -1,16 +1,14 @@
-import PyPDF2 as pdf
+import pdfplumber
 
 def check_kiid(file_name):
     # podajesz nazwe pliku i zwraca TRUE gdy tekst zaczyna sie od "kluczowe informacje dla inwestorow"
 
-    file = open(file_name,'rb')
-    reader = pdf.PdfFileReader(file)
-    pageObj = reader.getPage(0)
-    text = pageObj.extract_text()
-    text_ = text.lower()[1:200]
-    return "kluczowe informacje dla inwestorów" in text_   
+    
+    pdf = pdfplumber.open(file_name)
+    text = pdf.pages[0].extract_text().lower()[1:200]
+    return "kluczowe informacje dla inwestorów" in text
 
 if __name__ == "__main__":
     #test
-    file_name = "KIID_BNP_Paribas_DI_2022-09-21.pdf" 
+    file_name = "be4dbbf2-4e25-4053-87ba-52f13e939a48.pdf" 
     print(check_kiid(file_name))
