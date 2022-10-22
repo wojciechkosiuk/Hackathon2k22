c4da883672cc7fa43d89ff6c2c9039776fbbd100
Damian Skowroński <72418134+skowronskid@users.noreply.github.com>
2022-10-22 11:29:00 +0200

    added nazwa subfunduszu

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,18 +1,22 @@
 import PyPDF2 as pdf
 
 def extract(file_name):
-    # podajesz nazwe pliku i otrzymujesz string z calej zawartosci
+    # podajesz nazwe pliku i otrzymujesz string z calej zawartosci i NAZWE_SUBFUNDUSZU znaleziona w pliku
     # jest troche problem, ze wystepuja spacje dzielace niektore slowa (?)
     file = open(file_name,'rb')
     reader = pdf.PdfFileReader(file)
+    
+    
     
     full_text = ""
     for page_nr in range(reader.numPages):
         page = reader.getPage(page_nr)
         full_text += page.extract_text()
 
+    NAZWA_SUBFUNDUSZU = text.split("inwestycyjną.\n")[1].split("\n")[0]
+    
     full_text = full_text.replace('\n', ' ')
-    return full_text
+    return full_text, NAZWA_SUBFUNDUSZU
 
 
 
